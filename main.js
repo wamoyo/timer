e11ac49e063c569765bf286a7f1d56a42e362fe7
Costa Michailidis <costa@innovationbound.com>
2019-03-07 11:39:02 -0500

    interface kind of complete

---
+++ main.js
@@ -0,0 +1,22 @@
+!(function () {
+  "use strict"
+  window.addEventListener('load', function (event) {
+
+    var timer = document.getElementById('timer')
+    var hour2 = document.getElementById('hour-2')
+    var hour1 = document.getElementById('hour-1')
+    var colon2 = document.getElementById('colon-2')
+    var min2 = document.getElementById('min-2')
+    var min1 = document.getElementById('min-1')
+    var colon1 = document.getElementById('colon-1')
+    var sec2 = document.getElementById('sec-2')
+    var sec1 = document.getElementById('sec-1')
+    var controls = document.getElementById('controls')
+    var startStop = document.getElementById('start-stop')
+    var setReset = document.getElementById('set-reset')
+    var input = document.getElementById('input')
+
+    //
+
+  }, false)
+}());
